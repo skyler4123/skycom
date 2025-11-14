49f16845e981f5343ce83180c3bcccf0ae44f985
skyler <pkphung2001@gmail.com>
2025-11-15 03:39:00 +0700

    employee group seed

--- app/services/seed/employee_group_service.rb
+++ app/services/seed/employee_group_service.rb
@@ -5,6 +5,20 @@ class Seed::EmployeeGroupService
         name: "EmployeeGroup #{index}",
         description: Faker::Movie.quote
       )
+
+      # 1. Create the single Tag object and assign it to a variable
+      new_tag = Tag.create(
+        company: company,
+        name: "EMployee Group Tag #{employee_group.id}"
+      )
+
+      # 2. Add the single Tag object to the employee_group's tags collection
+      employee_group.tags << new_tag
+
+      # 3. Now you can safely call methods on the single new_tag object
+      new_tag.tag_appointments.where(appoint_to: employee_group).update(
+        value: "Value #{index}"
+      )
     end
   end
 end
