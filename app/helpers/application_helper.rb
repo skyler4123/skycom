e2aa1d8be422058345ff7114c668cc7a60826ea4
skyler <pkphung2001@gmail.com>
2025-11-15 17:00:17 +0700

    ServerData

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def stimulus_controller
+    [ controller_path.gsub("/", "--"), action_name ].join("--").dasherize
+  end
+
+  def server_data(json_data: nil, pagination: nil, flash: nil)
+    "
+      <script>
+        window.ServerData = {
+          data: #{ json_data&.html_safe || {} },
+          pagination: #{ pagination&.to_json&.html_safe || {} },
+          flash: #{ flash.to_hash.to_json.html_safe }
+        };
+      </script>
+    "
+  end
 end
