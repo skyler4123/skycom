940561d59cef56f53c82e8d87d4a38c3e6c19c9f
skyler <pkphung2001@gmail.com>
2025-11-16 14:11:35 +0700

    helper js

--- app/javascript/controllers/helpers.js
+++ app/javascript/controllers/helpers.js
@@ -1,3 +1,6 @@
+import Swal from 'sweetalert2';
+import dayjs from 'dayjs'
+
 export const isDefined = (x) => {
   return typeof x !== 'undefined'  
 }
@@ -69,4 +72,171 @@ export const avatar = () => {
 }
 export const email = () => {
   return Cookie('email')
+}
+
+
+
+
+// Function to open SweetAlert2 dialog based on a parent element
+export const openPopover = ({parentElement, html = "Dialog content", position = 'bottom-center-center', popupClass = ""}) => {
+  // Get the parent element's position and dimensions
+  const parentRect = parentElement.getBoundingClientRect();
+  const parentTop = parentRect.top;
+  const parentBottom = parentRect.bottom;
+  const parentLeft = parentRect.left;
+  const parentRight = parentRect.right;
+  const parentWidth = parentRect.width;
+  const parentHeight = parentRect.height;
+    
+  // Customize SweetAlert2 dialog
+  Swal.fire({
+    html: html,
+    position: 'top-start', // Use 'top-start' to position the dialog at the top-left corner
+    showConfirmButton: false,
+    showCloseButton: false,
+    // backdrop: false,
+    customClass: {
+      container: '!bg-transparent',
+      popup: 'swal2-container-custom ' + popupClass,
+      htmlContainer: '!p-0',
+    },
+    didOpen: (popupElement) => {
+      // Adjust the dialog's position based on the parent element
+      const swalContainer = document.querySelector('.swal2-container-custom');
+      swalContainer.style.position = 'absolute';
+      switch (position) {
+        case 'top-left':
+          swalContainer.style.top = `${parentTop}px`;
+          swalContainer.style.left = `${parentLeft}px`;
+          break;
+        case 'top-right':
+          swalContainer.style.top = `${parentTop}px`;
+          swalContainer.style.left = `${parentRight}px`;
+          break;
+        case 'top-center':
+          swalContainer.style.top = `${parentTop}px`;
+          swalContainer.style.left = `${parentLeft + parentWidth/2}px`;
+          break;
+
+        case 'bottom-left':
+          swalContainer.style.top = `${parentBottom}px`;
+          swalContainer.style.left = `${parentLeft}px`;
+          break;
+        case 'bottom-right':
+          swalContainer.style.top = `${parentBottom}px`;
+          swalContainer.style.left = `${parentRight}px`;
+          break;
+        case 'bottom-center':
+          swalContainer.style.top = `${parentBottom}px`;
+          swalContainer.style.left = `${parentLeft + parentWidth/2}px`;
+          break;
+
+        case 'left-center':
+          swalContainer.style.top = `${parentTop + parentHeight/2}px`;
+          swalContainer.style.left = `${parentLeft}px`;
+          break;
+        case 'right-center':
+          swalContainer.style.top = `${parentTop + parentHeight/2}px`;
+          swalContainer.style.left = `${parentRight}px`;
+          break;
+        case 'center-center':
+          swalContainer.style.top = `${parentTop + parentHeight/2}px`;
+          swalContainer.style.left = `${parentLeft + parentWidth/2}px`;
+          break;
+      }
+    },
+  });
+}
+
+export const openModal = ({html = "Model!", customClass = {}, options = {}}) => {
+  Swal.fire({
+    html: html,
+    showConfirmButton: false,
+    showCloseButton: false,
+    backdrop: true,
+    target: document.querySelector('main'), // Default target
+    customClass: {
+      container: '!bg-transparent',
+      popup: '!p-0 !bg-transparent !w-full',
+      htmlContainer: '!p-0 !overflow-visible',
+      ...customClass
+    },
+    ...options,
+  });
+}
+
+export const openDrawer = ({html = "Drawer!", position = "top-start", popupClass = ""}) => {
+  Swal.fire({
+    html: html,
+    position: position,
+    showConfirmButton: false,
+    showCloseButton: false,
+    backdrop: true,
+    showClass: {
+      popup: `
+        animate__animated
+        animate__fadeInUp
+        animate__faster
+      `
+    },
+    hideClass: {
+      popup: `
+        animate__animated
+        animate__fadeOutDown
+        animate__faster
+      `
+    },
+    customClass: {
+      container: '!p-0',
+      popup: 'swal2-container-custom h-screen',
+      htmlContainer: '!p-0',
+    },
+  });
+}
+
+export const openFlash = (html, option = {}) => {
+  Swal.fire({
+    position: "top",
+    html: html,
+    showConfirmButton: false,
+    timer: 3000,
+    backdrop: false,
+    customClass: {
+      container: '...1',
+      popup: '!p-0',
+      header: '...2',
+      title: '...3',
+      closeButton: '...',
+      icon: '...',
+      image: '...',
+      htmlContainer: '!p-0',
+      input: '...',
+      inputLabel: '...',
+      validationMessage: '...',
+      actions: '...',
+      confirmButton: '...',
+      denyButton: '...',
+      cancelButton: '...',
+      loader: '...5',
+      footer: '....6',
+      timerProgressBar: '....7',
+    },
+    ...option
+  });
+}
+
+export const pathname = () => {
+  return window.location.pathname
+}
+
+export const href = () => {
+  return window.location.href
+}
+
+export const origin = () => {
+  return window.location.origin
+}
+
+export const timeFormat = (time, format = "DD/MM/YYYY") => {
+  return dayjs(time).format(format)
 }
--- app/javascript/controllers/layout_controller.js
+++ app/javascript/controllers/layout_controller.js
@@ -33,8 +33,12 @@ export default class LayoutController extends ApplicationController {
     // avatar()
     if (isSignedIn()) {
       return `
-        <div>
-          <img class="w-10 h-10 rounded-full" src="${avatar()}" alt="Rounded avatar">
+        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+          ${avatar() ? 
+            `<img class="w-10 h-10 rounded-full" src="${avatar()}" alt="Rounded avatar">`
+          :
+            `<svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>`
+          }
         </div>
       `
     } else {
