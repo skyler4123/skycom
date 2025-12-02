c94eea7665936a727806f903f987a52355a450b7
skyler <pkphung2001@gmail.com>
2025-12-02 08:01:25 +0700

    apex + tabular

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,14 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sweetalert2" # @11.26.3
 pin "dayjs" # @1.11.19
 pin "stimulus-use" # @0.52.3
+pin "apexcharts" # @5.3.6
+pin "tabulator" # @0.2.40
+pin "best-globals" # @0.10.34
+pin "buffer" # @2.1.0
+pin "codenautas-xlsx" # @0.11.12
+pin "crypto" # @2.1.0
+pin "fs" # @2.1.0
+pin "js-to-html" # @1.0.11
+pin "like-ar" # @0.2.19
+pin "process" # @2.1.0
+pin "stream" # @2.1.0
