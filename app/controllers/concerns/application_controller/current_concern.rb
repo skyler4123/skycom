cdc0c811dadfdac0a9ba04217d1eaf84295cfbbc
skyler <pkphung2001@gmail.com>
2025-12-03 23:05:23 +0700

    cookies[:current_company_group_id]

--- app/controllers/concerns/application_controller/current_concern.rb
+++ app/controllers/concerns/application_controller/current_concern.rb
@@ -8,12 +8,12 @@ module ApplicationController::CurrentConcern
 
     # Get current_company_group_id from Cookie
     def set_current_company_group_id
-      if cookies[:current_company_group_id]
-        Current.company_group_id = cookies[:current_company_group_id]
-      else
-        Current.company_group_id = Current.user&.company_groups&.first&.id
-        cookies[:current_company_group_id] = Current.company_group_id
-      end
+      Current.company_group_id = params[:education_id] if params[:education_id].present?
+      Current.company_group_id = params[:hospital_id] if params[:hospital_id].present?
+      Current.company_group_id = params[:hotel_id] if params[:hotel_id].present?
+      Current.company_group_id = params[:restaurant_id] if params[:restaurant_id].present?
+      Current.company_group_id = params[:retail_id] if params[:retail_id].present?
+      cookies[:current_company_group_id] = Current.company_group_id
     end
   end
 end
