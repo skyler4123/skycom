e277f4e7a737739a9280170b2d1bd2657f1903c2
skyler <pkphung2001@gmail.com>
2025-12-14 03:42:04 +0700

    fix test

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Authentication", type: :feature, js: true do
-  let(:new_user_params) {
-    {
-      email: 'test@example.com',
-      password: 'Password@1234',
-      password_confirmation: 'Password@1234'
+  describe "Sign up with normal user" do
+    let(:new_user_params) {
+      {
+        email: 'test@example.com',
+        password: 'Password@1234',
+        password_confirmation: 'Password@1234'
+      }
     }
-  }
-  let(:created_user) { User.find_by(email: new_user_params[:email]) }
+    let(:created_user) { User.find_by(email: new_user_params[:email]) }
 
-  context "Sign up with normal user" do
     it "signs me in" do
       visit root_path
 
@@ -26,6 +26,25 @@ RSpec.feature "Authentication", type: :feature, js: true do
       expect(page).to have_button("Sign Out")
       expect(page).to have_selector('[role="avatar"]')
       expect(created_user).to be_truthy
+    end
+  end
+
+  describe "Sign in with normal user" do
+    let(:user) { create(:user) }
+    it "signs me in" do
+      debugger
+      visit root_path
+
+      click_on "Sign In"
+
+      within('form[role="Sign In Form"]') do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: 'Password@1234'
+        click_button "Sign In"
+      end
+
+      expect(page).to have_button("Sign Out")
+      expect(page).to have_selector('[role="avatar"]')
     end
   end
 end
