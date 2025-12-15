d1ac106380aa3ae3d01d649d40bf1c8f2629f45b
skyler <pkphung2001@gmail.com>
2025-12-16 00:34:46 +0700

    increase retry

--- spec/retry_helper.rb
+++ spec/retry_helper.rb
@@ -8,11 +8,11 @@ RSpec.configure do |config|
   config.display_try_failure_messages = true
 
   # Retry failed examples
-  config.default_retry_count = 2
+  config.default_retry_count = 5
 
   # run retry only on features
   config.around :each, :js do |ex|
-    ex.run_with_retry retry: 2
+    ex.run_with_retry retry: 5
   end
 
   # callback to be run between retries
