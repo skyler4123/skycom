81eb2ffec43974fb03cc0c84b02b5af41c8d9b0c
skyler <pkphung2001@gmail.com>
2025-12-21 09:16:43 +0700

    branch type

---
+++ app/javascript/types.js
@@ -0,0 +1,32 @@
+/**
+ * @global
+ * @typedef {Object} Branch
+ * @property {string} id
+ * @property {string} company_group_id
+ * @property {string|null} parent_company_id
+ * @property {string|null} category_id
+ * @property {string} name
+ * @property {string} description
+ * @property {string|null} code
+ * @property {string} lifecycle_status
+ * @property {string} workflow_status
+ * @property {string} ownership_type
+ * @property {string} business_type
+ * @property {string} currency
+ * @property {string} registration_number
+ * @property {string} vat_id
+ * @property {string|null} tax_id
+ * @property {string} timezone
+ * @property {string} address_line_1
+ * @property {string} city
+ * @property {string} postal_code
+ * @property {string} country
+ * @property {string} email
+ * @property {string} phone_number
+ * @property {string} website
+ * @property {number} employee_count
+ * @property {string} fiscal_year_end_month
+ * @property {string|null} discarded_at
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
