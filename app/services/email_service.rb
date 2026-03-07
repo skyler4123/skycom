87eee6538fa987792b9f8e94fc95e30d47e477f3
skyler <pkphung2001@gmail.com>
2026-03-07 15:47:32 +0700

    seed

---
+++ app/services/email_service.rb
@@ -0,0 +1,23 @@
+class EmailService
+  attr_reader :username, :full_domain, :domain, :top_level_domain
+
+  def initialize(email)
+    @email = email.to_s.strip.downcase
+    parse_email
+  end
+
+  private
+
+  def parse_email
+    return unless @email.include?('@')
+
+    # Destructuring the split array
+    @username, @full_domain = @email.split('@')
+
+    if @full_domain
+      domain_parts = @full_domain.split('.')
+      @domain = domain_parts.first
+      @top_level_domain = domain_parts.last
+    end
+  end
+end
