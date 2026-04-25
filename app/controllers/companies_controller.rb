bfa68a219ea6c71f59010979721537ef237c3ef7
skyler <pkphung2001@gmail.com>
2026-04-25 15:42:01 +0700

    feat: add CompaniesController with create action

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,19 @@
+class CompaniesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    company = current_user.companies.build(company_params)
+
+    if company.save
+      render json: { company: company }, status: :created
+    else
+      render json: { errors: company.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :business_type)
+  end
+end
