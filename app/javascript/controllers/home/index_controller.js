01bcf664fe29eac3ef55cecbef34065050990e05
skyler <pkphung2001@gmail.com>
2026-04-25 19:31:21 +0700

    feat(home): wire New Company button to open modal

--- app/javascript/controllers/home/index_controller.js
+++ app/javascript/controllers/home/index_controller.js
@@ -1,5 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
-
+import Home_NewModalController from "controllers/home/new_modal_controller"
 
 export default class Home_IndexController extends Controller {
+  openNewModal() {
+    openModal({
+      html: `<div data-controller="${identifier(Home_NewModalController)}"></div>`
+    })
+  }
 }
