1a3a9b5b725cebb12ac78fce5617c9203f4c6f50
skyler <pkphung2001@gmail.com>
2026-05-14 18:18:57 +0700

    remove cache concern

--- app/models/concerns/cache/records_concern.rb
+++ app/models/concerns/cache/records_concern.rb
@@ -10,46 +10,34 @@ module Cache::RecordsConcern
 
   # Class methods for fetching
   class_methods do
-    # Usage: Employee.cached_where(company_id: 'uuid', status: 'active')
+    # Usage: Employee.cached_where(company_id: 'uuid', expires_in: 1.hour)
     def cached_where(**filters)
-      # 1. Create the relation to generate the SQL
-      relation = where(**filters)
+      expires_in = filters.delete(:expires_in) || 5.minutes
 
-      # 2. Generate a unique key based on the SQL
-      sql_raw  = relation.to_sql.squish
-      sql_hash = Digest::SHA256.base64digest(sql_raw).tr("+/", "-_").first(12)
+      relation = where(filters)
+      sql_hash = Digest::SHA256.base64digest(relation.to_sql.squish).tr("+/", "-_").first(12)
+      cache_key = "#{model_name.plural.underscore}/q#{sql_hash}"
 
-      # Example Key: employees/queries/qR3V5X_xvZ2lj
-      cache_key = "#{model_name.plural.underscore}/queries/q#{sql_hash}"
-
-      # 3. Fetch the array of attribute hashes from Solid Cache
-      attributes_array = Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
-        # We execute the query and convert the result to an array of hashes
+      attributes_array = Rails.cache.fetch(cache_key, expires_in: expires_in) do
         relation.map(&:attributes)
       end
 
       return [] if attributes_array.blank?
-
-      # 4. Turn the raw hashes back into actual model objects
-      # .instantiate tells Rails the record is already persisted in the DB
       attributes_array.map { |attrs| instantiate(attrs) }
     end
 
-    # Usage: @user = User.cached_find(params[:id])
-    def cached_find(id)
+    # Usage: User.cached_find(id, expires_in: 10.minutes)
+    # Or: User.cached_find(id)
+    def cached_find(id, **options)
       return nil if id.blank?
 
-      cache_key = "#{model_name.plural}_#{id}"
+      expires_in = options.delete(:expires_in) || 5.minutes
+      cache_key  = "#{model_name.plural}_#{id}"
 
-      # Fetch the hash and re-hydrate into a new object
-      attributes = Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
-        # If it's a miss, hit the DB and return the attributes hash
+      attributes = Rails.cache.fetch(cache_key, expires_in: expires_in) do
         find_by(id: id)&.attributes
       end
 
-      # Turn the hash back into a model instance (without hitting DB)
-      # .instantiate is the "Senior Dev" way to create an object from
-      # a hash while marking it as "persisted" (not a new record).
       instantiate(attributes) if attributes.present?
     end
   end
