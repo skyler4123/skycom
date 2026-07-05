c8555d93672d20ce8d257e943e04561aeebd391d
skyler <pkphung2001@gmail.com>
2026-07-05 17:52:28 +0700

    merge migrations

--- db/migrate/20260105172236_create_attendance_months.rb
+++ db/migrate/20260105172236_create_attendance_months.rb
@@ -12,6 +12,7 @@ class CreateAttendanceMonths < ActiveRecord::Migration[8.0]
       t.integer :total_absent_days, default: 0
       t.integer :total_present_days, default: 0
       t.integer :total_records, default: 0
+      t.integer :total_deficit_minutes, default: 0
 
       t.integer :lifecycle_status
       t.integer :workflow_status
