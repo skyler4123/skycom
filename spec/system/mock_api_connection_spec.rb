16c7fe8db4dc4f2afbcb2fcfb1dd6d99c2f4f1de
skyler <pkphung2001@gmail.com>
2026-07-26 19:58:36 +0700

    fix test

--- spec/system/mock_api_connection_spec.rb
+++ spec/system/mock_api_connection_spec.rb
@@ -1,22 +1,20 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "net/http"
-require "json"
 
 MOCK_API_PING_URL = ENV["MOCK_API_PING_URL"] || Rails.application.credentials.mock_api_ping_url || "http://localhost:4000/api/v1/ping"
 
 RSpec.describe "Mock API Server Connection", type: :request do
-  let(:uri) { URI(MOCK_API_PING_URL) }
+  let(:connection) { Faraday.new(url: MOCK_API_PING_URL) }
 
   it "responds to ping endpoint" do
-    response = Net::HTTP.get_response(uri)
-    expect(response).to be_a(Net::HTTPOK)
-    expect(response.code.to_i).to eq(200)
+    response = connection.get
+    expect(response.success?).to be true
+    expect(response.status).to eq(200)
   end
 
   it "returns expected JSON body" do
-    response = Net::HTTP.get_response(uri)
+    response = connection.get
     body = JSON.parse(response.body)
 
     expect(body).to include(
