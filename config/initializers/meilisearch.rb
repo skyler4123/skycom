7cd8a6fe7dc0a0acc5b0854c9c0ca409f42af1ad
skyler <pkphung2001@gmail.com>
2026-08-28 15:42:24 +0700

    feat(search): add Meilisearch integration for user search
    - Add meilisearch-rails gem
    - Add User::SearchConcern with email/username/name/phone searchable attributes
    - Add Meilisearch Docker service (v1.53.1) with healthcheck and volume
    - Add Meilisearch initializer with env-configurable host and API key

---
+++ config/initializers/meilisearch.rb
@@ -0,0 +1,4 @@
+Meilisearch::Rails.configuration = {
+  meilisearch_url: ENV.fetch('MEILISEARCH_HOST', 'http://localhost:7700'),
+  meilisearch_api_key: ENV.fetch('MEILISEARCH_API_KEY', 'skycom_master_key_password_2026')
+}
