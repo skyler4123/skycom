efe3e6e2708aa93bf0fdf288fde68ca4cfa038ab
skyler <pkphung2001@gmail.com>
2026-09-16 17:47:36 +0700

    feat: store sidebar_groups in company settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,6 +3,7 @@ class Setting < ApplicationRecord
   include TagConcern
 
   store_accessor :metadata, :sidebar_items
+  store_accessor :metadata, :sidebar_groups
 
   # --- Enums ---
   enum :lifecycle_status, LIFECYCLE_STATUS, prefix: true
